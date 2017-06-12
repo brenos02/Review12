3c0c090177dd4f0f9593c171c5b822ba353eb7bb
Breno Sallouti <brenosallouti@icloud.com>
2017-06-12 10:31:43 -0300

    Não estou conseguindo, pode me ajudar por favor

--- Review12/main.cpp
+++ Review12/main.cpp
@@ -4,12 +4,24 @@
 //
 //  Created by Breno Sallouti on 09/06/17.
 //  Copyright © 2017 Breno Sallouti. All rights reserved.
-//
+//- Get number from user and detect if it is a prime number
 
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    float a;
+    bool prime;
+    prime = true;
+    
+    std::cin>> a;
+   
+    if (a == prime) {
+        prime =true;
+        std::cout<<"It is a prime number";
+    }
+    else{
+        std::cout<<"It not a prime number";
+        prime =false;
+    }
     return 0;
 }
